a6acad845c17bc47b1fbef75d5da0ebc25a992f1
John Ryan <jtigger@pm.me>
2022-12-04 14:56:27 -0800

    Solve day 3, part 1: calculate compartment priority

---
+++ day03/sort-rucksacks.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"unicode"
+)
+
+func runeInBoth(one, other string) rune {
+	for _, oneChar := range one {
+		for _, otherChar := range other {
+			if oneChar == otherChar {
+				return oneChar
+			}
+		}
+	}
+	return 0
+}
+
+func priority(char rune) int {
+	if unicode.IsUpper(char) {
+		return int(char-'A') + 27
+	}
+	if unicode.IsLower(char) {
+		return int(char-'a') + 1
+	}
+	log.Panicf("unrecognized rune: %c (%d)", char, char)
+	return 0
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	total := 0
+	for scanner.Scan() {
+		rucksack := scanner.Text()
+		comp1 := rucksack[:len(rucksack)/2]
+		comp2 := rucksack[len(rucksack)/2:]
+
+		common := runeInBoth(comp1, comp2)
+		pri := priority(common)
+		total += pri
+
+		// fmt.Printf("Rucksack:\n  left  = %s\n  right = %s\n  common = %c (%d)\n", comp1, comp2, common, pri)
+	}
+	fmt.Printf("Total: %d\n", total)
+}
