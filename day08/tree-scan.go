6c4f872161fe87aa884bce397905ca107e2bdac4
John Ryan <jtigger@pm.me>
2022-12-12 08:24:55 -0800

    Solve day 8, part 2: calculate best "scenic score"

--- day08/tree-scan.go
+++ day08/tree-scan.go
@@ -6,44 +6,52 @@ import (
 	"os"
 )
 
-func isOccludedNorth(grid [][]int, rowIdx, colIdx int) bool {
-	treeHeight := grid[rowIdx][colIdx]
-	for gridRow := rowIdx - 1; gridRow >= 0; gridRow-- {
-		if grid[gridRow][colIdx] >= treeHeight {
-			return true
+func isOccludedNorth(grid [][]int, treeRow, treeCol int) (treesVisible int, isOccluded bool) {
+	treeHeight := grid[treeRow][treeCol]
+	for gridRow := treeRow - 1; gridRow >= 0; gridRow-- {
+		if grid[gridRow][treeCol] >= treeHeight {
+			// e.g. tree at row 2, occluded by tree at row 0; treesVisible = 2
+			return treeRow - gridRow, true
 		}
 	}
-	return false
+	return treeRow, false
 }
 
-func isOccludedSouth(grid [][]int, rowIdx, colIdx int) bool {
-	treeHeight := grid[rowIdx][colIdx]
-	for gridRow := rowIdx + 1; gridRow < len(grid); gridRow++ {
-		if grid[gridRow][colIdx] >= treeHeight {
-			return true
+func isOccludedSouth(grid [][]int, treeRow, treeCol int) (treesVisible int, isOccluded bool) {
+	treeHeight := grid[treeRow][treeCol]
+	for gridRow := treeRow + 1; gridRow < len(grid); gridRow++ {
+		if grid[gridRow][treeCol] >= treeHeight {
+			// e.g. tree at row 8, occluded by tree at row 10; treesVisible = 2
+			return gridRow - treeRow, true
 		}
 	}
-	return false
+	// e.g. tree at row 8 on a grid with 12 rows; treesVisible = 3
+	return (len(grid) - 1) - treeRow, false
 }
 
-func isOccludedEast(grid [][]int, rowIdx, colIdx int) bool {
-	treeHeight := grid[rowIdx][colIdx]
-	for gridCol := colIdx + 1; gridCol < len(grid[rowIdx]); gridCol++ {
-		if grid[rowIdx][gridCol] >= treeHeight {
-			return true
+func isOccludedEast(grid [][]int, treeRow, treeCol int) (treesVisible int, isOccluded bool) {
+	treeHeight := grid[treeRow][treeCol]
+	for gridCol := treeCol + 1; gridCol < len(grid[treeRow]); gridCol++ {
+		if grid[treeRow][gridCol] >= treeHeight {
+			return gridCol - treeCol, true
 		}
 	}
-	return false
+	return (len(grid[treeRow]) - 1) - treeCol, false
 }
 
-func isOccludedWest(grid [][]int, rowIdx, colIdx int) bool {
-	treeHeight := grid[rowIdx][colIdx]
-	for gridCol := colIdx - 1; gridCol >= 0; gridCol-- {
-		if grid[rowIdx][gridCol] >= treeHeight {
-			return true
+func isOccludedWest(grid [][]int, treeRow, treeCol int) (treesVisible int, isOccluded bool) {
+	treeHeight := grid[treeRow][treeCol]
+	for gridCol := treeCol - 1; gridCol >= 0; gridCol-- {
+		if grid[treeRow][gridCol] >= treeHeight {
+			return treeCol - gridCol, true
 		}
 	}
-	return false
+	return treeCol, false
+}
+
+type Visibility struct {
+	Visible     bool
+	ScenicScore int
 }
 
 func main() {
@@ -59,19 +67,28 @@ func main() {
 	}
 
 	numVisible := 0
-	var visGrid [][]bool
+	maxScenicScore := 0
+	var visGrid [][]Visibility
 	for rowIdx, row := range heightGrid {
-		visGrid = append(visGrid, make([]bool, len(row)))
+		visGrid = append(visGrid, make([]Visibility, len(row)))
 		for colIdx, _ := range row {
-			visNorth := !isOccludedNorth(heightGrid, rowIdx, colIdx)
-			visSouth := !isOccludedSouth(heightGrid, rowIdx, colIdx)
-			visEast := !isOccludedEast(heightGrid, rowIdx, colIdx)
-			visWest := !isOccludedWest(heightGrid, rowIdx, colIdx)
-			visGrid[rowIdx][colIdx] = visNorth || visSouth || visEast || visWest
-			if visGrid[rowIdx][colIdx] {
+			treesToNorth, occludedNorth := isOccludedNorth(heightGrid, rowIdx, colIdx)
+			treesToSouth, occludedSouth := isOccludedSouth(heightGrid, rowIdx, colIdx)
+			treesToEast, occludedEast := isOccludedEast(heightGrid, rowIdx, colIdx)
+			treesToWest, occludedWest := isOccludedWest(heightGrid, rowIdx, colIdx)
+
+			visGrid[rowIdx][colIdx] = Visibility{
+				!occludedNorth || !occludedSouth || !occludedEast || !occludedWest,
+				treesToNorth * treesToSouth * treesToEast * treesToWest,
+			}
+			if visGrid[rowIdx][colIdx].Visible {
 				numVisible++
+			}
+			if visGrid[rowIdx][colIdx].ScenicScore > maxScenicScore {
+				maxScenicScore = visGrid[rowIdx][colIdx].ScenicScore
 			}
 		}
 	}
 	fmt.Printf("Number of visible trees: %d\n", numVisible)
+	fmt.Printf("Max scenic score: %d\n", maxScenicScore)
 }
